6c81ca05a043dfc4e715848cb3c882115cee46f0
faryal16 <bintefarzana1992@gmail.com>
2025-08-24 18:32:31 +0500

    Tracing with Gemini

---
+++ 10_tracing.py
@@ -0,0 +1,69 @@
+import os
+from dotenv import load_dotenv
+from typing import cast
+import chainlit as cl
+from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel,set_tracing_disabled,set_trace_processors
+from agents.tracing.processors import BatchTraceProcessor, ConsoleSpanExporter, default_processor
+from agents.tracing.processor_interface import TracingExporter
+from agents.tracing.spans import Span
+from agents.tracing.traces import Trace
+
+# Load the environment variables from the .env file
+load_dotenv()
+
+# make custom class for show trace by cutomerized
+
+class CustomConsoleSpanExporter(TracingExporter):
+    def export(self, items:list[Trace | Span]):
+        for item in items:
+            if isinstance(item, Trace):
+                print(f"[Trace] ID: {item.trace_id} | Name: {item.name}")
+            elif item.span_data.type == "generation":
+                usage = item.span_data.usage or {}
+                model = item.span_data.model
+                user_input = item.span_data.input or []
+                output = item.span_data.output or []
+
+                print("🧠 Model Used:", model)
+                print("📥 Input Tokens:", usage.get("input_tokens", "N/A"))
+                print("📤 Output Tokens:", usage.get("output_tokens", "N/A"))
+
+                if user_input:
+                    print("🙋 User Asked:", user_input[-1].get("content", "N/A"))
+                if output:
+                    print("🤖 Bot Replied:", output[0].get("content", "N/A"))
+# ---------------
+exporter = CustomConsoleSpanExporter()
+processor= BatchTraceProcessor(exporter)
+
+set_trace_processors([processor])
+
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+# Check if the API key is present; if not, raise an error
+if not gemini_api_key:
+    raise ValueError("GEMINI_API_KEY is not set. Please ensure it is defined in your .env file.")
+
+
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=external_client
+)
+
+
+
+agent = Agent(
+    name="Gemini Agent",
+    instructions="You are a helpful agent.",
+    model=model
+)
+
+    
+result = Runner.run_sync(agent, input="hi", )
+print(result.final_output)
